cbd740420af1657e28a415b599800c16ec421ffd
Lucas <tilucast@gmail.com>
2021-07-06 13:24:02 -0300

    3:11 hour mark on bros code java tutorial

---
+++ src/OOP/Car.java
@@ -0,0 +1,17 @@
+package OOP;
+
+public class Car {
+    String make = "Chevrolet";
+    String model = "Covert";
+    int year = 2020;
+    String color = "red";
+    double price = 20000;
+
+    void drive(){
+        System.out.println("You are driving.");
+    }
+
+    void brake(){
+        System.out.println("You have stopped the car.");
+    }
+}
---
+++ src/OOP/Main.java
@@ -0,0 +1,19 @@
+package OOP;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Car corvet = new Car();
+//        Car corvet2 = new Car();
+//        System.out.println(corvet);
+//        System.out.println(corvet2);
+//
+//        corvet.drive();
+
+        Human human = new Human("Rulio", 24, 69.9);
+        Human human2 = new Human("Benis", 20, 89.9);
+
+        human.eat();
+        human2.drink();
+    }
+}
